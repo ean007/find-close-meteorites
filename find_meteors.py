dfd2aa06b7a0ac5fc95e99cb8bc9fff57ea1a1ad
iwsmith <iwsmith@nx-vi.local>
2020-07-17 21:07:20 -0400

    Ran black over the find_meteors.py file

--- find_meteors.py
+++ find_meteors.py
@@ -14,25 +14,27 @@ def calc_dist(lat1, lon1, lat2, lon2):
 
     return 6372.8 * 2 * math.asin(math.sqrt(h))
 
+
 def get_dist(meteor):
-    return meteor.get('distance', math.inf)
+    return meteor.get("distance", math.inf)
 
-my_loc=(-33.224, -70.688)
 
-meteor_resp=requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-meteor_data= meteor_resp.json()
+my_loc = (-33.224, -70.688)
+
+meteor_resp = requests.get("https://data.nasa.gov/resource/gh4g-9sfh.json")
+meteor_data = meteor_resp.json()
 
 for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                    float(meteor['reclong']),
-                                    my_loc[0],
-                                    my_loc[1])
+    if not ("reclat" in meteor and "reclong" in meteor):
+        continue
+    meteor["distance"] = calc_dist(
+        float(meteor["reclat"]), float(meteor["reclong"]), my_loc[0], my_loc[1]
+    )
 
 meteor_data.sort(key=get_dist)
 
-n=0
+n = 0
 
 for meteor in meteor_data[0:10]:
     print(f"{meteor_data[n]}\n")
-    n=n+1
+    n = n + 1
